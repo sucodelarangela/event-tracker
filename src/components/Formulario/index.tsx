d2829359f1d515ac46f58f7957f52cd7eb64a1e9
Angela Caldas <aes.caldas@gmail.com>
2023-04-08 11:18:24 -0300

    refactor: updating listaDeEventos atom with Recoil hook useSetRecoilState

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,88 +1,98 @@
 import React, { useState } from 'react';
-import { IEvento } from '../../interfaces/IEvento';
 import style from './Formulario.module.scss';
+import { obterId } from '../../util';
+import { useSetRecoilState } from 'recoil';
+import { IEvento } from '../../interfaces/IEvento';
+import { listaDeEventosState } from '../../state/atom';
 
-const Formulario: React.FC<{ aoSalvar: (evento: IEvento) => void }> = ({ aoSalvar }) => {
-  const [descricao, setDescricao] = useState('')
-  const [dataInicio, setDataInicio] = useState('')
-  const [horaInicio, setHoraInicio] = useState('')
-  const [dataFim, setDataFim] = useState('')
-  const [horaFim, setHoraFim] = useState('')
+const Formulario: React.FC = () => {
+  // Criando um setState com Recoil para atualizar a listaDeEventos e re-renderizar o componente novamente
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
 
-  const montarData = (data:string, hora: string) => {
-    const dataString = data.slice(0, 10)
-    return new Date(`${dataString}T${hora}`)
-  }
+  const [descricao, setDescricao] = useState('');
+  const [dataInicio, setDataInicio] = useState('');
+  const [horaInicio, setHoraInicio] = useState('');
+  const [dataFim, setDataFim] = useState('');
+  const [horaFim, setHoraFim] = useState('');
+
+  const montarData = (data: string, hora: string) => {
+    const dataString = data.slice(0, 10);
+    return new Date(`${dataString}T${hora}`);
+  };
 
   const submeterForm = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    aoSalvar({
+    event.preventDefault();
+    const evento = {
+      id: obterId(),
       descricao,
       inicio: montarData(dataInicio, horaInicio),
       fim: montarData(dataFim, horaFim),
       completo: false
-    })
-    setDescricao('')
-    setDataInicio('')
-    setHoraInicio('')
-    setDataFim('')
-    setHoraFim('')
-  }
+    };
+    // Atualizando a listaDeEventos
+    setListaDeEventos(listaAntiga => [...listaAntiga, evento]);
+
+    setDescricao('');
+    setDataInicio('');
+    setHoraInicio('');
+    setDataFim('');
+    setHoraFim('');
+  };
   return (<form className={style.Formulario} onSubmit={submeterForm}>
     <h3 className={style.titulo}>Novo evento</h3>
 
     <label>Descrição</label>
-    <input 
-      type="text" 
+    <input
+      type="text"
       name="descricao"
       id="descricao"
       className={style.input}
-      onChange={evento => setDescricao(evento.target.value)} 
-      placeholder="Descrição" value={descricao} 
+      onChange={evento => setDescricao(evento.target.value)}
+      placeholder="Descrição" value={descricao}
       autoComplete="off"
       required />
 
-      <label>Data de início</label>
-      <div className={style.inputContainer}>
-        <input 
-          type="date" 
-          name="dataInicio"
-          className={style.input}
-          onChange={evento => setDataInicio(evento.target.value)} 
-          value={dataInicio}
-          required />
-        <input 
-          type="time" 
-          name="horaInicio"
-          className={style.input}
-          onChange={evento => setHoraInicio(evento.target.value)} 
-          value={horaInicio}
-          required />
-      </div>
+    <label>Data de início</label>
+    <div className={style.inputContainer}>
+      <input
+        type="date"
+        name="dataInicio"
+        className={style.input}
+        onChange={evento => setDataInicio(evento.target.value)}
+        value={dataInicio}
+        required />
+      <input
+        type="time"
+        name="horaInicio"
+        className={style.input}
+        onChange={evento => setHoraInicio(evento.target.value)}
+        value={horaInicio}
+        required />
+    </div>
 
-      <label>Data de término</label>
-      <div className={style.inputContainer}>
-        <input 
-          type="date" 
-          name="dataFim"
-          className={style.input}
-          onChange={evento => setDataFim(evento.target.value)} 
-          value={dataFim}
-          required />
-        <input 
-          type="time" 
-          name="horaFim"
-          className={style.input}
-          onChange={evento => setHoraFim(evento.target.value)} 
-          value={horaFim}
-          required />
-      </div>
+    <label>Data de término</label>
+    <div className={style.inputContainer}>
+      <input
+        type="date"
+        name="dataFim"
+        className={style.input}
+        onChange={evento => setDataFim(evento.target.value)}
+        value={dataFim}
+        required />
+      <input
+        type="time"
+        name="horaFim"
+        className={style.input}
+        onChange={evento => setHoraFim(evento.target.value)}
+        value={horaFim}
+        required />
+    </div>
 
     <button className={style.botao}>
       Salvar
     </button>
 
-  </form>)
-}
+  </form>);
+};
 
-export default Formulario
+export default Formulario;
